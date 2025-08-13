6099311ffb21f2f553366377e4767963faa2aea2
Thomas Kosch <kosch90@icloud.com>
2025-08-13 18:49:22 +0200

    Included pictures

---
+++ paths.py
@@ -0,0 +1,14 @@
+import os
+
+base = 'stimuli/ai_generated/man_filtered'
+ethnicities = ['asian', 'black', 'latino', 'white']
+
+paths = []
+for group in ethnicities:
+    folder = os.path.join(base, group)
+    for file in os.listdir(folder):
+        if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+            paths.append(f'"{folder}/{file}"')
+
+# Print list for JS
+print("const imagePaths = [\n  " + ",\n  ".join(paths) + "\n];")
