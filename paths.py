b91953f0dedbf6d1e78ffe04626456641a508dce
Teodora <teodora.mitrevska@ifi.lmu.de>
2025-08-19 10:43:18 +0200

    female stimuli added to base

--- paths.py
+++ paths.py
@@ -1,14 +1,20 @@
 import os
 
-base = 'stimuli/ai_generated/man_filtered'
+bases = [
+    'stimuli/ai_generated/man_filtered',
+    'stimuli/ai_generated/woman_filtered'
+]
 ethnicities = ['asian', 'black', 'latino', 'white']
 
 paths = []
-for group in ethnicities:
-    folder = os.path.join(base, group)
-    for file in os.listdir(folder):
-        if file.lower().endswith(('.jpg', '.jpeg', '.png')):
-            paths.append(f'"{folder}/{file}"')
+for base in bases:
+    for group in ethnicities:
+        folder = os.path.join(base, group)
+        if not os.path.exists(folder):
+            continue
+        for file in os.listdir(folder):
+            if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+                paths.append(f'"{folder}/{file}"')
 
 # Print list for JS
 print("const imagePaths = [\n  " + ",\n  ".join(paths) + "\n];")
